d03a0fe48dca094d4a6bf0393226bfd8cd969f24
Divya Tiwari <divyatiwari1112@gmail.com>
2021-12-06 06:21:25 +0530

    Implemented PID class

---
+++ scripts/controllers.py
@@ -0,0 +1,38 @@
+from _typeshed import Self
+import numpy as np
+import rospy 
+import maplotlib.pyplot as plt
+
+
+class PID_controller:
+    def __init__(self,sat,simTStep) -> None:
+        self.output = 0
+        self.error = 0
+        self.error_sum = 0
+        self.saturation = sat
+        self.dt = simTStep
+
+    def PID(self,Kp, Ki, Kd, state_val, curr_val,speed=True,angle=False):
+
+        curr_error = state_val - curr_val
+        self.error_sum = self.error + curr_error*self.dt
+        error_diff = (curr_error - self.error)/self.dt
+
+        a = Kp*self.error + Ki*(self.error_sum) + Kd*error_diff
+
+        if speed:
+            if a > self.saturation:
+                a = self.saturation
+                self.error_sum = 0
+            elif a < 0:
+                a = 0
+
+        if angle:
+            if abs(a) > self.saturation:
+                self.error_sum = 0
+                if a > 0:
+                    a = self.saturation
+                else:
+                    a = -1*self.saturation
+                
+        return a
